51a8f4ca838decc0ef14505bbc3953fbbd633069
Young <younglou1995@gmail.com>
2019-10-16 14:24:06 +0800

    @Value

--- src/main/java/com/majiang/community/controller/AuthorizeController.java
+++ src/main/java/com/majiang/community/controller/AuthorizeController.java
@@ -4,9 +4,12 @@ import com.majiang.community.dto.AccessTokenDTO;
 import com.majiang.community.dto.GithubUser;
 import com.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.swing.plaf.ProgressBarUI;
 
 @Controller
 public class AuthorizeController {
@@ -14,15 +17,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.redirect.uri}")
+    private String githubRedirectUri;
+
+    @Value("${github.client.id}")
+    private String githubClientId;
+
+    @Value("${github.client.secret}")
+    private String githubClientSecret;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
-        accessTokenDTO.setClient_id("cd1f051064fb18319fce");
-        accessTokenDTO.setClient_secret("ee7df96fdd6f768199cdf75af9a03846e0b92bdb");
+        accessTokenDTO.setRedirect_uri(githubRedirectUri);
+        accessTokenDTO.setClient_id(githubClientId);
+        accessTokenDTO.setClient_secret(githubClientSecret);
         String acceessToken = githubProvider.getAcceessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(acceessToken);
         System.out.println(user.getName());
